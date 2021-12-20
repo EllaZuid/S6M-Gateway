cec2f6716f2f4bc5bd3a6688cb6af46fb408bced
Ella Zuidema <ella.zuidema@student.fontys.nl>
2021-12-20 22:23:55 +0100

    Revert "Auto stash before merge of "main" and "origin/main""
    This reverts commit 3ac5aa165efce200ce89773c28fa56f3a77e61f0.

--- src/main/java/S6M/Gateway/GatewayApplication.java
+++ src/main/java/S6M/Gateway/GatewayApplication.java
@@ -2,13 +2,16 @@ package S6M.Gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-//@EnableEurekaClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
@@ -23,8 +26,7 @@ public class GatewayApplication {
 				registry.addMapping("/**")
 						.allowedOrigins("*")
 						.allowedMethods("GET","POST", "PUT", "DELETE", "OPTIONS")
-						.allowedHeaders("*")
-						.exposedHeaders("*");
+						.allowedHeaders("*");
 			}
 		};
 	}
